5a6b1bc57e85c3cdf99b10d03ea074068e294cd8
cjoynerw <cjoynerw@gmail.com>
2020-12-09 14:42:13 -0600

    refactored code to make DRY, stored open and close functionality in functions within  variables

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 'use strict';
 
+// Global Variables
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+// Reused Functions
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+// Opens Modal with each of the buttons
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// Closes Modal if X btn is clicked or outside of modal is clicked
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
