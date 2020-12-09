6e3b227dba8298e3e11d81e544654fcdb9e28693
cjoynerw <cjoynerw@gmail.com>
2020-12-09 14:37:12 -0600

    created functionality to open and close btns for modal

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', function () {
     console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
