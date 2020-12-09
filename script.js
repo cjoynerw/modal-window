0c54ddc40555528fa8589402dfbce999a05033fd
cjoynerw <cjoynerw@gmail.com>
2020-12-09 14:23:59 -0600

    initial commit & created global variables

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+  });
